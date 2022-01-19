f88e135fc9a240895a4501ca0ff5d83d9336673f
mrsharma1710 <56424543+mrsharma1710@users.noreply.github.com>
2022-01-19 10:48:04 +0530

    Commit

---
+++ POC2/src/main/java/com/neosoft/Poc2Application.java
@@ -0,0 +1,14 @@
+package com.neosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Poc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc2Application.class, args);
+	}
+
+}
